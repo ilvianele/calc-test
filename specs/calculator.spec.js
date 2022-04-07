8d3f448790b163b70ad0d0939ada0e08e8cd4e30
ilvianele <helmarsel@gmail.com>
2022-04-07 15:54:59 +0300

    added tests after lesson

--- specs/calculator.spec.js
+++ specs/calculator.spec.js
@@ -4,10 +4,36 @@ describe('Calculator 100% coverage', () => {
     expect(calculator).toBeDefined();
     expect(typeof calculator).toBe('function');
   });
+  test ('6 + 8 = 14', () => {
+    expect(calculator(6, '+', 8)).toBe(14)
+  });
+  test ('6 - 8 = -2', () => {
+    expect(calculator(6, '-', 8)).toBe(-2)
+  });
+  test ('6 * 8 = 48', () => {
+    expect(calculator(6, '*', 8)).toBe(48)
+  });
+  test ('48 / 8 = 6', () => {
+    expect(calculator(48, '/', 8)).toBe(6)
+  });
+  test('throw error if operand A is not a number', () => {
+    expect(() => calculator('?', '*', 8)).toThrow();
+  });
+  test('throw error if operand B is not a number', () => {
+    expect(() => calculator(6, '*', '?')).toThrow();
+  });
+  test('throw error if operator is not valid', () => {
+    expect(() => calculator(6, '?', 8)).toThrow();
+  });
   test.each`
-    a      | op     | b      | expected
-    ${2}   | ${'*'} | ${2}   | ${4}
-    ${2}   | ${'&'} | ${2}   | ${'error'}
+    a       | op     | b       | expected
+    ${6}    | ${'*'} | ${8}    | ${48}
+    ${48}   | ${'/'} | ${8}    | ${6}
+    ${6}    | ${'+'} | ${8}    | ${14}
+    ${6}    | ${'-'} | ${8}    | ${-2}
+    ${6}    | ${'&'} | ${8}    | ${'error'}
+    ${'aa'} | ${'+'} | ${8}    | ${'error'}
+    ${6}    | ${'+'} | ${'aa'} | ${'error'}
   `('$a $op $b = $expected', ({ a, op, b, expected }) => {
     if (expected === 'error') {
       expect(() => calculator(a, op, b)).toThrow();
